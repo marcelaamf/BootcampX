2b6cec54d44ea6ea23a85d025d07ca85e55950ad
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-03-10 22:47:09 -0700

    Refactored teachers.js and students.js to avoid sql injection

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vitorpratafernandes',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id=teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1 
+ORDER BY teachers.name;
+`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(` ${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
